36626b6422d0193b6b49bb05ecd808b1c12efe85
alex_s168 <63254202+SuperCraftAlex@users.noreply.github.com>
2023-07-04 12:55:38 +0200

    Add files via upload

--- py-efl.py
+++ py-efl.py
@@ -1,4 +1,6 @@
 from sys import exit
+import os.path
+from sys import argv
 
 DEBUG = False
 EDEBUG = False
@@ -12,6 +14,7 @@ functions = []
 
 
 using_flow_l = False
+using_arr_l = False
 
 
 def debug(*m):
@@ -75,9 +78,12 @@ def prepare(data):
         exit(-1)
 
     for line in lines:
+        if line[0] == "(" and line[-1] == ")":
+            continue
+    
         a = line.split("=")
         b = line.split("+=")
-    
+
         if len(a) > 0:
             if len(b) > 0:
                 if len(a[0]) > len(b[0]):
@@ -108,6 +114,8 @@ def prepare(data):
             fargl = left.split("(")[1].split(")")[0].split(",")
             for farg in fargl:
                 farg = farg.strip()
+
+            fargl = list(filter(lambda x: x != "", fargl))
 
             found = False
             for func in functions:
@@ -193,7 +201,7 @@ class StringLiteral:
         self.val = val
 
 
-def parse(s):
+def _parse(s):
     global variables
     global functions
     global using_flow_l
@@ -207,7 +215,7 @@ def parse(s):
     if len(s) == 0:
             return ""
 
-    debug("parsing",s)
+    debug("parsing @"+s+"@:")
 
     # === weird shit ===
     if s[0] == "(":
@@ -296,20 +304,80 @@ def parse(s):
 
             edebug("character:",c,"fin:",fin)
 
-            if c == "\"" and fin == 1:
-                parsing_str = not parsing_str
-                if not parsing_str:
-                    wasstring = True
-            elif c == '(' and fin == 1:
+            #if c == "\"" and fin == 1:
+            #    parsing_str = not parsing_str
+            #    if not parsing_str:
+            #        wasstring = True
+            if c == '(' and fin == 1:
                 parsing_fn = False
                 parsing_farg = True
                 fname = parsingv
                 parsingv = ""
             elif c == ')' and fin == 0:
-                fargs.append(parsingv.strip())
+                if len(parsingv.strip()) > 0:
+                    fargs.append(parsingv.strip())
                 parsing_farg = False
                 parsingv = ""
                 # done parsing
+
+                if using_arr_l:
+                    if fname == "filter" and len(fargs) == 2:   # applies filter [arg 1] on array [arg 2]
+                        a = fargs[0].split("=>")
+                        filtv = a[0]
+                        filtf = "=>".join(a[1:])
+
+                        newl = []
+                        for x in parse(fargs[1].strip()):
+                            oldvars = variables.copy()
+                            variables.append([filtv, x])
+                            v = parse(filtf.strip())
+                            variables = oldvars
+                            if v: newl.append(x)
+
+                        return newl
+
+                    if fname == "map" and len(fargs) == 2: # applies operation on every element of array
+                        a = fargs[0].split("=>")
+                        filtv = a[0]
+                        filtf = "=>".join(a[1:])
+
+                        newl = []
+                        for x in parse(fargs[1].strip()):
+                            oldvars = variables.copy()
+                            variables.append([filtv, x])
+                            newl.append(parse(filtf.strip()))
+                            variables = oldvars
+
+                        return newl
+
+                    if fname == "reduce" and len(fargs) == 2: # for example sums all elements
+                        a = fargs[0].split("=>")[0].strip()
+                        if a[0] != "(" or a[-1] != ")":
+                            print("invalid usage of reduce function!")
+                            exit()
+                        a = a[1:-1]
+                        b = a.split(",")
+                        if len(b) != 2:
+                            print("invalid usage of reduce function!")
+                            exit()
+
+                        vara = b[0].strip()
+                        varb = b[1].strip()
+
+                        arr = parse(fargs[1].strip())
+
+                        if len(arr) == 0:
+                            return []
+
+                        val = arr[0]
+                        for x in arr[1:]:
+                            oldvars = variables.copy()
+                            variables.append([vara, val])
+                            variables.append([varb, x])
+                            val = parse("=>".join(fargs[0].split("=>")[1:]).strip())
+                            variables = oldvars
+
+                        return val
 
                 if using_flow_l:
                     if fname == "do":
@@ -317,25 +385,16 @@ def parse(s):
                             parse(arg)
                         return 0
 
-                    if fname == "after": # execute a after b (and returns b)
-                        if len(fargs) != 2:
-                            print("invalid arguments")
-                            exit()
+                    if fname == "after" and len(fargs) == 2: # execute a after b (and returns b)
                         val = parse(fargs[1])
                         parse(fargs[0])
                         return val
 
-                    if fname == "before": # execute a before b (and returns b)
-                        if len(fargs) != 2:
-                            print("invalid arguments")
-                            exit()
+                    if fname == "before" and len(fargs) == 2: # execute a before b (and returns b)
                         parse(fargs[0])
                         return parse(fargs[1])
 
-                    if fname == "if": # [boolean] [if true] [if false (optional)]
-                        if len(fargs) < 2 or len(fargs) > 3:
-                            print("invalid arguments")
-                            exit()
+                    if fname == "if" and len(fargs) > 1 and len(fargs) < 4: # [boolean] [if true] [if false (optional)]
                         c = rs(parse(fargs[0]))
                         debug("condition result:",c)
                         if c:
@@ -345,13 +404,10 @@ def parse(s):
                         else:
                             return False
 
-                    if fname == "with": # [var name (string)]=[value], ops...   and returns result of last expression
-                        if len(fargs) < 2:
-                            print("invalid arguments")
-                            exit()
+                    if fname == "with" and len(fargs) > 1: # [var name (string)]=[value], ops...   and returns result of last expression
                         a = fargs[0].split("=")
-                        oldvars = variables
-                        variables.append((a[0], parse(a[1].strip())))
+                        oldvars = variables.copy()
+                        variables.append((a[0], parse("=".join(a[1:]).strip())))
 
                         for exp in fargs[1:]:
                             val = parse(exp.strip())
@@ -365,7 +421,7 @@ def parse(s):
                 found = False
                 argn = []
                 for func in functions:
-                    if func[0] == fname:
+                    if func[0] == fname and len(fargs) == len(func[1]):
                         found = True
                         if len(func) > 3:
                             ispycode = True
@@ -381,11 +437,11 @@ def parse(s):
                             variables.append([arg, fargs[argi]])
 
                 if not found:
-                    print("no function named ", fname, "!")
+                    print("no function named ", fname, " with", len(fargs), "argument(s)!")
                     exit()
 
                 if ispycode:
-                    variables = oldvars
+                    variables = oldvars.copy()
                     
                     debug("standart library function call: ", fname, " args (parsed): ")
                     evars = {}
@@ -444,12 +500,19 @@ def parse(s):
         return rs(parse(sp[0].strip())) and rs(parse(sp[1].strip()))
 
     return s
-    
 
-import os.path
-from sys import argv
+def parse(s):
+    val = _parse(s)
+    if type(val) == list:
+        new = []
+        for v in val:
+            new.append(rs(v))
+        return new
+    debug("- val: ", val)
+    return val
 
-if len(argv) < 2 or len(argv) > 3:
+
+if len(argv) < 2:
     print("invalid arguments!")
     print("usage: [file] [init code = main(0)]")
     exit()
@@ -485,6 +548,8 @@ for lib in pathv.split(";"):
         functions.append(["int", ["a"], "", "int(a)"])
         functions.append(["float", ["a"], "", "float(a)"])
         functions.append(["list", ["a"], "", "list(a)"])
+        functions.append(["ascii", ["a"], "", "ord(a)"])
+        functions.append(["chr", ["a"], "", "chr(int(a))"])
     elif lib == "math.l":
         functions.append(["mul", ["a", "b"], "", "a * b"])
         functions.append(["add", ["a", "b"], "", "a + b"])
@@ -499,6 +564,7 @@ for lib in pathv.split(";"):
         functions.append(["input", [], "", "input()"])
         functions.append(["print", ["a"], "", "print(a)"])
     elif lib == "arr.l":
+        using_arr_l = True
         functions.append(["join", ["arr", "c"], "", "c.join(arr)"])
         functions.append(["sort", ["arr"], "", "arr.sort()"])
         functions.append(["get", ["index", "arr"], "", "arr[index]"])
@@ -523,4 +589,4 @@ else:
     if not found:
         print("no function called main!")
     else:
-        print(rs(parse("main(0)")))
+        print(rs(parse("main()")))
